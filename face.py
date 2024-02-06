d7d30e9515ea51313a0cdebd0ee81b123614bab5
Sandeep VISSAPRAGADA <sandeepvissa2002@gmail.com>
2024-02-06 17:05:21 +0000

    v1

---
+++ face.py
@@ -0,0 +1,23 @@
+from transformers import DetrImageProcessor, DetrForObjectDetection
+import torch
+from PIL import Image
+import requests
+
+image = Image.open("images.jpeg")
+
+# you can specify the revision tag if you don't want the timm dependency
+processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+
+inputs = processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+
+target_sizes = torch.tensor([image.size[::-1]])
+results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.8)[0]
+
+for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+    box = [round(i, 2) for i in box.tolist()]
+    print(
+            f"Detected {model.config.id2label[label.item()]} with confidence "
+            f"{round(score.item(), 3)} at location {box}"
+    )
